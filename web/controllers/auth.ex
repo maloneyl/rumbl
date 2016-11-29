2d99bae1892a83ec395538a5fa6874a7575e5ef9
Maloney Liu <maloney@buyapowa.com>
2016-11-29 14:11:23 +0000

    log user in upon sign-up
    (p.82)

--- web/controllers/auth.ex
+++ web/controllers/auth.ex
@@ -8,6 +8,13 @@ defmodule Rumbl.Auth do
   def call(conn, repo) do
     user_id = get_session(conn, :user_id)
     user = user_id && repo.get(Rumbl.User, user_id)
-    assign(conn, :current_user, user)
+    assign(conn, :current_user, user) # the Plug.Conn struct has a field called assigns
+  end
+
+  def login(conn, user) do
+    conn
+    |> assign(:current_user, user)
+    |> put_session(:user_id, user.id)
+    |> configure_session(renew: true) # tells Plug to send session cookie back to client with different identifier
   end
 end
