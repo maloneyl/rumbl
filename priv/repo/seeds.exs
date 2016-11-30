41aa1547f7b7ca9c3189ece45679067467e6408e
Maloney Liu <maloney@buyapowa.com>
2016-11-30 13:53:17 +0000

    add seeds to create categories
    (p.110)

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -9,3 +9,13 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+
+alias Rumbl.Repo
+alias Rumbl.Category
+
+# we've prevented the same category being added twice
+# with unique constraint on name in the database (see migrations)
+# so it'll just blow up if we do have duplicated categories here
+for category <- ~w(Action Drama Romance Comedy Sci-fi) do
+  Repo.insert!(%Category{name: category})
+end
