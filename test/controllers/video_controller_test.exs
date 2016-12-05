e0bd4a06ebc5d54d6e0c7b3adca9f0a365f57166
Maloney Liu <maloney@buyapowa.com>
2016-12-05 11:27:57 +0000

    use ExUnit tag to determine whether to log in our user
    (p.139)

--- test/controllers/video_controller_test.exs
+++ test/controllers/video_controller_test.exs
@@ -1,28 +1,32 @@
 defmodule Rumbl.VideoControllerTest do
   use Rumbl.ConnCase
 
-  # THESE NOW BREAK BECAUSE THESE EXPECT A CONNECTION WITHOUT A USER LOGGED IN
-  # test "requires user authentication on all actions", %{conn: conn} do
-  #   Enum.each([
-  #     get(conn, video_path(conn, :new)),
-  #     get(conn, video_path(conn, :index)),
-  #     get(conn, video_path(conn, :show, "123")),
-  #     get(conn, video_path(conn, :edit, "123")),
-  #     put(conn, video_path(conn, :update, "123", %{})),
-  #     post(conn, video_path(conn, :create,  %{})),
-  #     delete(conn, video_path(conn, :delete, "123")),
-  #   ], fn conn ->
-  #     assert html_response(conn, 302)
-  #     assert conn.halted
-  #   end)
-  # end
-
-  setup do
-    user = insert_user(username: "max")
-    conn = assign(build_conn(), :current_user, user)
-    {:ok, conn: conn, user: user}
+  test "requires user authentication on all actions", %{conn: conn} do
+    Enum.each([
+      get(conn, video_path(conn, :new)),
+      get(conn, video_path(conn, :index)),
+      get(conn, video_path(conn, :show, "123")),
+      get(conn, video_path(conn, :edit, "123")),
+      put(conn, video_path(conn, :update, "123", %{})),
+      post(conn, video_path(conn, :create,  %{})),
+      delete(conn, video_path(conn, :delete, "123")),
+    ], fn conn ->
+      assert html_response(conn, 302)
+      assert conn.halted
+    end)
   end
 
+  setup %{conn: conn} = config do
+    if username = config[:login_as] do
+      user = insert_user(username: "max")
+      conn = assign(build_conn(), :current_user, user)
+      {:ok, conn: conn, user: user}
+    else
+      :ok
+    end
+  end
+
+  @tag login_as: "max" # when you specify a tag, ExUnit makes the info available within the setup block via callbacks
   test "lists all user's videos on index", %{conn: conn, user: user} do
     user_video = insert_video(user, title: "funny cats")
     other_video = insert_video(insert_user(username: "other"), title: "another video")
