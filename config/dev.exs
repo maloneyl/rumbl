a89d83959d214054e28538028a670085a6178315
Maloney Liu <maloneyl@users.noreply.github.com>
2017-01-25 18:53:05 +0800

    add JS to watch video
    (p.164)

--- config/dev.exs
+++ config/dev.exs
@@ -11,7 +11,7 @@ config :rumbl, Rumbl.Endpoint,
   debug_errors: true,
   code_reloader: true,
   check_origin: false,
-  watchers: [node: ["node_modules/brunch/bin/brunch", "watch", "--stdin",
+  watchers: [node: ["node_modules/brunch/bin/brunch", "watch", "--stdin", # --stdin makes brunch abort when Phoenix shuts down
                     cd: Path.expand("../", __DIR__)]]
 
 
