deae1f32293e618ab46d459585292e1f3fde1bbd
Maloney Liu <maloneyl@users.noreply.github.com>
2017-05-29 17:34:08 +0100

    annotate videos (client)
    (p.185)

--- web/static/js/video.js
+++ web/static/js/video.js
@@ -16,11 +16,41 @@ let Video = {
     let postButton = document.getElementById("msg-submit")
     let vidChannel = socket.channel("videos:" + videoId) // identifier for our topic
 
-    vidChannel.on("ping", ({count}) => console.log("PING", count))
+    postButton.addEventListener("click", e => {
+      let payload = {body: msgInput.value, at: Player.getCurrentTime()}
+
+      // request-response-style messaging over a socket connection
+      vidChannel.push("new_annotation", payload)
+                .receive("error", e => console.log(e)) // not a true synchronous operation
+
+      msgInput.value = ""
+    })
+
+    vidChannel.on("new_annotation", (resp) => {
+      this.renderAnnotation(msgContainer, resp)
+    })
 
     vidChannel.join()
       .receive("ok", resp => console.log("joined the video channel", resp))
       .receive("error", reason => console.log("join failed", reason))
+  },
+
+  // escape user input before injecting values into the page
+  esc(str){
+    let div = document.createElement("div")
+    div.appendChild(document.createTextNode(str))
+    return div.innerHTML
+  },
+
+  renderAnnotation(msgContainer, {user, body, at}){
+    let template = document.createElement("div")
+    template.innerHTML = `
+    <a href="#" data-seek="${this.esc(at)}">
+      <b>${this.esc(user.username)}</b>: ${this.esc(body)}
+    </a>
+    `
+    msgContainer.appendChild(template)
+    msgContainer.scrollTop = msgContainer.scrollHeight
   }
 }
 
