c87be7929ad93a1606e931eef21428addbc50fc6
Maloney Liu <maloneyl@users.noreply.github.com>
2017-09-24 17:58:19 +0100

    child worker restart strategies
    (p.208)

--- lib/rumbl.ex
+++ lib/rumbl.ex
@@ -16,6 +16,14 @@ defmodule Rumbl do
       # Start your own worker by calling: Rumbl.Worker.start_link(arg1, arg2, arg3)
       # worker(Rumbl.Worker, [arg1, arg2, arg3]),
       worker(Rumbl.Counter, [5]) # our arg here is the initial_value
+      # By default, child processes have a restart strategy of :permanent,
+      # which we could write explicitly like worker(Rumbl.Counter, [5], restart: :permanent).
+      # A supervisor will always restart a :permanent GerServer, whether the process
+      # crashed or terminated gracefully.
+      # :temporary == never restart (useful if restarting is unlikely to resolve the problem or doesn't make sense)
+      # :transient == restart only if terminated abnormally, with an exit reason other than :normal, :shutdown or {:shutdown, term}
+      # OTP will only restart an application max_restarts times in max_seconds;
+      # by default, Elixir will allow 3 restarts in 5 seconds, but this pair of options can be configured.
     ]
 
     # See http://elixir-lang.org/docs/stable/elixir/Supervisor.html
