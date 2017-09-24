f8aeeb34f8901d76e3c9d3896a5dbcb6917f1099
Maloney Liu <maloneyl@users.noreply.github.com>
2017-09-24 17:21:19 +0100

    update Counter to use GenServer
    (p.204)

--- lib/rumbl/counter.ex
+++ lib/rumbl/counter.ex
@@ -1,53 +1,41 @@
 defmodule Rumbl.Counter do
+  use GenServer # generic server
+
   # CLIENT (INTERFACE)
   # Serves at the API and exists only to send messages
   # to the process that does the work
   #
   # async; send a message and don't bother to await any reply
-  def inc(pid), do: send(pid, :inc)
+  def inc(pid), do: GenServer.cast(pid, :inc)
 
-  def dec(pid), do: send(pid, :dec)
+  def dec(pid), do: GenServer.cast(pid, :dec)
 
-  def val(pid, timeout \\ 5000) do
-    ref = make_ref() # create a value guaranteed to be globally unique
-    send(pid, {:val, self(), ref}) # self is our pid
-
-    # after sending the message to the counter above,
-    # we block the caller process while waiting for a response
-    receive do
-      # ^ref means we match only tuples that have that exact ref,
-      # not reassigning the value of ref
-      # i.e. we match only responses related to our explicit request
-      {^ref, val} -> val
-    # if no match in a given period, we exit the current process
-    # with the :timeout reason code
-    after timeout -> exit(:timeout)
-    end
+  def val(pid) do
+    GenServer.call(pid, :val)
   end
 
   # OTP requires a start_link function
   def start_link(initial_val) do
-    {:ok, spawn_link(fn -> listen(initial_val) end)}
-    # returns {:ok, pid}, where pid identifies the spawned process;
-    # the spawned process then calls listen, which listens for messages and processes them
+    GenServer.start_link(__MODULE__, initial_val) # spawns a new process and invokes `init`
   end
 
   # SERVER (IMPLEMENTATION)
   # A process that recursively loops, 
   # processing a message and sending the updates state to itself
   #
-  defp listen(val) do
-    # blocks to wait for a message
-    # then process the trivial :inc, :dec and :val messages
-    # then call listen again with the updated state
-    receive do
-      :inc -> listen(val + 1)
-      :dec -> listen(val - 1)
-      {:val, sender, ref} ->
-        send sender, {ref, val}
-        listen(val)
-    end
-    # this function is tail recursive, meaning it optimizes to a loop instead of a function call;
-    # in Elixir, processes are incredibly cheap so this is a great way to manage state
+  def init(initial_val) do
+    {:ok, initial_val}
+  end
+
+  def handle_cast(:inc, val) do
+    {:noreply, val + 1}
+  end
+
+  def handle_cast(:dec, val) do
+    {:noreply, val - 1}
+  end
+
+  def handle_call(:val, _from, val) do
+    {:reply, val, val}
   end
 end
